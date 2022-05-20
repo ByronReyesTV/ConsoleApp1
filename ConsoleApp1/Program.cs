b1a7a0018c449cc1737fb65d89385c362197ddf2
reyes221093 <102719907+reyes221093@users.noreply.github.com>
2022-05-20 14:04:39 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+String [] array_name = new string [6];
+array_name[0] = "Jemmie";
+array_name[1] = "Jessica";
+array_name[2] = "Ahl";
+array_name[3] = "alea";
+array_name[4] = "luke";
+array_name[5] = "Arcilla";
+
+foreach (String name in array_name)
+{
+    Console.WriteLine(name);
+}
